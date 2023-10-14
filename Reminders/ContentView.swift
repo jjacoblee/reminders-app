1c80a930d75037a7ac08e29f93157cc6b997877c
Jacob Lee <jacoblee21@live.com>
2023-10-14 17:22:00 -0500

    format, update the repeat cadence input

--- Reminders/ContentView.swift
+++ Reminders/ContentView.swift
@@ -102,149 +102,157 @@ struct ContentView: View {
     }
 }
     
-    struct ModalView: View {
+struct ModalView: View {
+    
+    @Binding var isModalPresented: Bool
+    @ObservedObject var reminderData: ReminderData // Use the ReminderData object
+    
+    var reminderToEdit: Reminder?
+    
+    
+    // Reminder fields
+    @State private var name = ""
+    @State private var startDate = Date()
+    @State private var startTime = Date()
+    @State private var endDate = Date()
+    @State private var endTime = Date()
+    @State private var isRepeatOn = false
+    @State private var selectedRepeatOption = "hour"
+    @State private var isRepeatDaysOn = false
+    @State private var selectedRepeatDays: [String] = []
+    @State private var isEndDateAndTimeEnabled = false
+    @State private var repeatEvery = "1"
+    
+    let repeatOptions = ["second", "hour", "day", "week"]
+    let abbreviatedDaysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    
+    
+    var body: some View {
         
-        @Binding var isModalPresented: Bool
-        @ObservedObject var reminderData: ReminderData // Use the ReminderData object
-        
-        var reminderToEdit: Reminder?
-        
-        
-        // Reminder fields
-        @State private var name = ""
-        @State private var startDate = Date()
-        @State private var startTime = Date()
-        @State private var endDate = Date()
-        @State private var endTime = Date()
-        @State private var isRepeatOn = false
-        @State private var selectedRepeatOption = "hour"
-        @State private var isRepeatDaysOn = false
-        @State private var selectedRepeatDays: [String] = []
-        @State private var isEndDateAndTimeEnabled = false
-        
-        let repeatOptions = ["hour", "day", "week", "month", "year"]
-        let abbreviatedDaysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-        
-        
-        var body: some View {
+        NavigationView {
             
-            NavigationView {
-                
-                ScrollView{
-                    VStack {
-                        VStack(spacing: 20) {
-                            // Name
-                            TextField("Name", text: $name)
+            ScrollView{
+                VStack {
+                    VStack(spacing: 20) {
+                        // Name
+                        TextField("Name", text: $name)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        
+                        // Start Date and Time
+                        DatePicker("Start Date", selection: $startDate, in: Date()..., displayedComponents: .date)
+                        DatePicker("Start Time", selection: $startTime, in: Date()..., displayedComponents: .hourAndMinute)
+                        
+                        // End Date and Time
+                        Toggle("End Date and Time", isOn: $isEndDateAndTimeEnabled)
+                        
+                        if isEndDateAndTimeEnabled {
+                            DatePicker("End Date", selection: $endDate, in: startDate..., displayedComponents: .date)
+                            DatePicker("End Time", selection: $endTime, in: Date()..., displayedComponents: .hourAndMinute)
+                        }
+                        // Repeat Cadence
+                        HStack {
+                            Text("Repeat Every")
+                                .font(.body)
+                                .foregroundColor(.primary)
+                            
+                            TextField("", text: $repeatEvery)
+                                .keyboardType(.numberPad)
+                                .frame(width: 80)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                             
-                            
-                            // Start Date and Time
-                            DatePicker("Start Date", selection: $startDate, in: Date()..., displayedComponents: .date)
-                            DatePicker("Start Time", selection: $startTime, in: Date()..., displayedComponents: .hourAndMinute)
-                            
-                            // End Date and Time
-                            Toggle("End Date and Time", isOn: $isEndDateAndTimeEnabled)
-                            
-                            if isEndDateAndTimeEnabled {
-                                DatePicker("End Date", selection: $endDate, in: startDate..., displayedComponents: .date)
-                                DatePicker("End Time", selection: $endTime, in: Date()..., displayedComponents: .hourAndMinute)
-                            }
-                            // Repeat
-                            Toggle("Repeat Cadence", isOn: $isRepeatOn)
-                            
-                            if isRepeatOn {
-                                Picker("Repeat Cadence", selection: $selectedRepeatOption) {
-                                    ForEach(repeatOptions, id: \.self) { option in
-                                        Text(option)
-                                    }
+                            Picker("Unit", selection: $selectedRepeatOption) {
+                                ForEach(repeatOptions, id: \.self) { option in
+                                    Text(option)
                                 }
-                                .pickerStyle(SegmentedPickerStyle())
                             }
-                            
-                            // Repeat Days
-                            Toggle("Repeat Days", isOn: $isRepeatDaysOn)
-                            
-                            if isRepeatDaysOn {
-                                VStack {
-                                    List {
-                                        ForEach(abbreviatedDaysOfWeek, id: \.self) { day in
-                                            Toggle(day, isOn: Binding(
-                                                get: {
-                                                    self.selectedRepeatDays.contains(day)
-                                                },
-                                                set: { newValue in
-                                                    if newValue {
-                                                        self.selectedRepeatDays.append(day)
-                                                    } else {
-                                                        if let index = self.selectedRepeatDays.firstIndex(of: day) {
-                                                            self.selectedRepeatDays.remove(at: index)
-                                                        }
+                            .pickerStyle(SegmentedPickerStyle())
+                        }
+                        
+                        // Repeat Days
+                        Toggle("Repeat Days", isOn: $isRepeatDaysOn)
+                        
+                        if isRepeatDaysOn {
+                            VStack {
+                                List {
+                                    ForEach(abbreviatedDaysOfWeek, id: \.self) { day in
+                                        Toggle(day, isOn: Binding(
+                                            get: {
+                                                self.selectedRepeatDays.contains(day)
+                                            },
+                                            set: { newValue in
+                                                if newValue {
+                                                    self.selectedRepeatDays.append(day)
+                                                } else {
+                                                    if let index = self.selectedRepeatDays.firstIndex(of: day) {
+                                                        self.selectedRepeatDays.remove(at: index)
                                                     }
                                                 }
-                                            ))
-                                        }
+                                            }
+                                        ))
                                     }
-                                    
-                                    .background(Color(.systemBackground)) // Add a background color
-                                }.frame(minHeight: 380)
-                            }
-                            
-                            
-                            Button(action: {
-                                // Check if we are editing an existing reminder
-                                if let reminder = reminderToEdit {
-                                    // Find the reminder in the array and update it
-                                    if let index = reminderData.reminders.firstIndex(where: { $0.id == reminder.id }) {
-                                        reminderData.reminders[index].title = name
-                                        // Update other fields as necessary
-                                    }
-                                } else {
-                                    // Save the reminder as a new entry
-                                    let newReminder = Reminder(title: name, active: true) // Customize with user input
-                                    reminderData.reminders.append(newReminder)
                                 }
-                                reminderData.saveReminders()
-
-                                // Dismiss the modal
-                                self.isModalPresented.toggle()
-                            }) {
-                                Text("Submit")
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                            }
-
+                                
+                                .background(Color(.systemBackground)) // Add a background color
+                            }.frame(minHeight: 380)
                         }
-                        .padding()
+                        
+                        
+                        Button(action: {
+                            // Check if we are editing an existing reminder
+                            if let reminder = reminderToEdit {
+                                // Find the reminder in the array and update it
+                                if let index = reminderData.reminders.firstIndex(where: { $0.id == reminder.id }) {
+                                    reminderData.reminders[index].title = name
+                                    // Update other fields as necessary
+                                }
+                            } else {
+                                // Save the reminder as a new entry
+                                let newReminder = Reminder(title: name, active: true) // Customize with user input
+                                reminderData.reminders.append(newReminder)
+                            }
+                            reminderData.saveReminders()
+
+                            // Dismiss the modal
+                            self.isModalPresented.toggle()
+                        }) {
+                            Text("Submit")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+
                     }
+                    .padding()
                 }
-                .navigationBarItems(trailing: Button(action: {
-                    // Dismiss the modal when the "X" button is tapped
-                    self.isModalPresented.toggle()
-                }) {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.blue)
-                        .font(.headline)
-                })
-                .navigationBarTitle("Event Details")
-                .onAppear {
-                    // Load reminder details if we are editing an existing reminder
-                    if let reminder = reminderToEdit {
-                        self.name = reminder.title
-                        //TODO: Update other fields
-                    }
+            }
+            .navigationBarItems(trailing: Button(action: {
+                // Dismiss the modal when the "X" button is tapped
+                self.isModalPresented.toggle()
+            }) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.blue)
+                    .font(.headline)
+            })
+            .navigationBarTitle("Event Details")
+            .onAppear {
+                // Load reminder details if we are editing an existing reminder
+                if let reminder = reminderToEdit {
+                    self.name = reminder.title
+                    //TODO: Update other fields
                 }
             }
         }
     }
+}
     
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
     
     
     
