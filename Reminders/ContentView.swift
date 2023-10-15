7d4a63bde038581e22c2bc30c0419299bd3f6b63
Jacob Lee <jacoblee21@live.com>
2023-10-14 20:21:33 -0500

    add timed events to reminders

--- Reminders/ContentView.swift
+++ Reminders/ContentView.swift
@@ -1,21 +1,56 @@
 import SwiftUI
 
+enum DayOfWeek: Int, CaseIterable, Identifiable, Codable {
+    case Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat
+
+    var id: Int { self.rawValue }
+    
+    var shortName: String {
+        switch self {
+        case .Sun: return "Sun"
+        case .Mon: return "Mon"
+        case .Tue: return "Tue"
+        case .Wed: return "Wed"
+        case .Thu: return "Thu"
+        case .Fri: return "Fri"
+        case .Sat: return "Sat"
+        }
+    }
+    
+    // Custom encoder
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+
+    // Custom decoder
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(Int.self)
+        guard let value = DayOfWeek(rawValue: rawValue) else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid DayOfWeek rawValue")
+        }
+        self = value
+    }
+}
 
 struct Reminder: Identifiable, Codable { // Make Reminder Codable
     var id = UUID()
     var title: String
-    var startDate: Date
     var startTime: Date
-    var endDate: Date?
     var endTime: Date?
     var repeatEvery: String
     var repeatUnit: String
-    var repeatDays: [String]
+    var repeatDays: [DayOfWeek]
     var active: Bool
+    
+    var countdown: TimeInterval = 0
 }
+
 
 class ReminderData: ObservableObject {
     @Published var reminders: [Reminder] = []
+    var timers: [UUID: Timer] = [:]
     
     init() {
         loadReminders()
@@ -39,6 +74,90 @@ class ReminderData: ObservableObject {
         saveReminders()
     }
     
+    func startCountdowns() {
+        let calendar = Calendar.current
+
+        for index in reminders.indices where reminders[index].active {
+            let reminder = reminders[index]
+            let dayOfWeek = calendar.component(.weekday, from: Date()) // 1 = Sunday, 7 = Saturday
+
+            if !reminder.repeatDays.isEmpty && !reminder.repeatDays.contains(where: { $0.rawValue == dayOfWeek }) {
+                continue
+            }
+
+            let current = Date()
+            let start = combineDateAndTime(date: Date(), time: reminder.startTime)
+            if let end = reminder.endTime, current > end {
+                continue
+            }
+            if current < start {
+                continue
+            }
+
+            let repeatInSeconds = convertRepeatToSeconds(repeatEvery: reminder.repeatEvery, repeatUnit: reminder.repeatUnit)
+
+            // Initialize the countdown value when starting the timer
+            reminders[index].countdown = repeatInSeconds
+
+            timers[reminder.id]?.invalidate()
+            timers[reminder.id] = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                // Reduce the countdown
+                var updatedReminder = self.reminders[index]
+                updatedReminder.countdown -= 1
+
+                if updatedReminder.countdown <= 0 {
+                    updatedReminder.countdown = repeatInSeconds
+                }
+
+                self.reminders[index] = updatedReminder
+
+                if let end = reminder.endTime, Date() > end {
+                    self.timers[reminder.id]?.invalidate()
+                    self.timers.removeValue(forKey: reminder.id)
+                }
+            }
+        }
+    }
+
+    
+    func stopCountdowns() {
+        for timer in timers.values {
+            timer.invalidate()
+        }
+        timers.removeAll()
+    }
+    
+    private func combineDateAndTime(date: Date, time: Date) -> Date {
+        let calendar = Calendar.current
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: time)
+        return calendar.date(bySettingHour: timeComponents.hour!, minute: timeComponents.minute!, second: timeComponents.second!, of: date)!
+    }
+    
+    private func convertRepeatToSeconds(repeatEvery: String, repeatUnit: String) -> TimeInterval {
+        guard let value = Int(repeatEvery) else { return 0 }
+        switch repeatUnit {
+        case "sec":
+            return TimeInterval(value)
+        case "min":
+            return TimeInterval(value * 60)
+        case "hour":
+            return TimeInterval(value * 3600)
+        case "day":
+            return TimeInterval(value * 3600 * 24)
+        default:
+            return 0
+        }
+    }
+    
+}
+
+extension TimeInterval {
+    func formattedCountdown() -> String {
+        let hours = Int(self) / 3600
+        let minutes = (Int(self) % 3600) / 60
+        let seconds = Int(self) % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
 }
 
 struct ContentView: View {
@@ -61,13 +180,27 @@ struct ContentView: View {
                                     selectedReminder = reminder
                                     isModalPresented.toggle()
                                 }
-                            Spacer() // Add spacing to push the toggle to the right
+                            Spacer()
+                            
+                            // Countdown Text
+                            Text(reminder.countdown.formattedCountdown())
+                                .foregroundColor(.gray) // or any other color you prefer
+                                .font(.callout)         // or any other font style you prefer
+                            
+                            Spacer() // Add additional spacing if you want more gap between countdown and toggle
+                            
                             Toggle("", isOn: Binding(
                                 get: { reminder.active },
                                 set: { newValue in
                                     // Update the active status of the reminder
                                     if let index = reminderData.reminders.firstIndex(where: { $0.id == reminder.id }) {
                                         reminderData.reminders[index].active = newValue
+                                        if newValue {
+                                            self.reminderData.startCountdowns()
+                                        }
+                                        else{
+                                            self.reminderData.stopCountdowns()
+                                        }
                                     }
                                 }
                             ))
@@ -107,6 +240,7 @@ struct ContentView: View {
             .onAppear {
                 // Load reminders when the ContentView appears
                 self.reminderData.loadReminders()
+                self.reminderData.startCountdowns()
             }
         }
     }
@@ -121,19 +255,17 @@ struct ModalView: View {
     
     
     // Reminder fields
-    @State private var name = ""
-    @State private var startDate = Date()
+    @State private var name = "New Event"
     @State private var startTime = Date()
-    @State private var endDate = Date()
     @State private var endTime = Date()
-    @State private var selectedRepeatOption = "hour"
+    @State private var selectedRepeatOption = "min"
     @State private var isRepeatDaysOn = false
-    @State private var selectedRepeatDays: [String] = []
-    @State private var isEndDateAndTimeEnabled = false
+    @State private var selectedRepeatDays: [DayOfWeek] = []
+    @State private var isEndTimeEnabled = false
     @State private var repeatEvery = "1"
     
-    let repeatOptions = ["second", "hour", "day", "week"]
-    let abbreviatedDaysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    let repeatOptions = ["sec", "min", "hour", "day", "week"]
+    let abbreviatedDaysOfWeek = DayOfWeek.allCases
     
     
     var body: some View {
@@ -149,14 +281,12 @@ struct ModalView: View {
                         
                         
                         // Start Date and Time
-                        DatePicker("Start Date", selection: $startDate, in: Date()..., displayedComponents: .date)
                         DatePicker("Start Time", selection: $startTime, in: Date()..., displayedComponents: .hourAndMinute)
                         
                         // End Date and Time
-                        Toggle("End Date and Time", isOn: $isEndDateAndTimeEnabled)
+                        Toggle("End Time", isOn: $isEndTimeEnabled)
                         
-                        if isEndDateAndTimeEnabled {
-                            DatePicker("End Date", selection: $endDate, in: startDate..., displayedComponents: .date)
+                        if isEndTimeEnabled {
                             DatePicker("End Time", selection: $endTime, in: Date()..., displayedComponents: .hourAndMinute)
                         }
                         // Repeat Cadence
@@ -180,12 +310,12 @@ struct ModalView: View {
                         
                         // Repeat Days
                         Toggle("Repeat Days", isOn: $isRepeatDaysOn)
-                        
+
                         if isRepeatDaysOn {
                             VStack {
                                 List {
                                     ForEach(abbreviatedDaysOfWeek, id: \.self) { day in
-                                        Toggle(day, isOn: Binding(
+                                        Toggle(day.shortName, isOn: Binding(
                                             get: {
                                                 self.selectedRepeatDays.contains(day)
                                             },
@@ -201,7 +331,6 @@ struct ModalView: View {
                                         ))
                                     }
                                 }
-                                
                                 .background(Color(.systemBackground)) // Add a background color
                             }.frame(minHeight: 380)
                         }
@@ -213,30 +342,27 @@ struct ModalView: View {
                                 // Find the reminder in the array and update it
                                 if let index = reminderData.reminders.firstIndex(where: { $0.id == reminder.id }) {
                                     reminderData.reminders[index].title = name
-                                    reminderData.reminders[index].startDate = startDate
                                     reminderData.reminders[index].startTime = startTime
-                                    reminderData.reminders[index].endDate = isEndDateAndTimeEnabled ? endDate : nil
-                                    reminderData.reminders[index].endTime = isEndDateAndTimeEnabled ? endTime : nil
+                                    reminderData.reminders[index].endTime = isEndTimeEnabled ? endTime : nil
                                     reminderData.reminders[index].repeatEvery =  repeatEvery
                                     reminderData.reminders[index].repeatUnit = selectedRepeatOption
-                                    reminderData.reminders[index].repeatDays = isRepeatDaysOn ? selectedRepeatDays : abbreviatedDaysOfWeek
+                                    reminderData.reminders[index].repeatDays = isRepeatDaysOn ? selectedRepeatDays : []
                                 }
                             } else {
                                 // Save the reminder as a new entry
                                 let newReminder = Reminder(
                                     title: name,
-                                    startDate: startDate,
                                     startTime: startTime,
-                                    endDate: isEndDateAndTimeEnabled ? endDate : nil,
-                                    endTime: isEndDateAndTimeEnabled ? endTime : nil,
+                                    endTime: isEndTimeEnabled ? endTime : nil,
                                     repeatEvery:  repeatEvery,
                                     repeatUnit: selectedRepeatOption,
-                                    repeatDays: isRepeatDaysOn ? selectedRepeatDays : abbreviatedDaysOfWeek,
+                                    repeatDays: isRepeatDaysOn ? selectedRepeatDays : [],
                                     active: true
                                 ) // Customize with user input
                                 reminderData.reminders.append(newReminder)
                             }
                             reminderData.saveReminders()
+                            reminderData.startCountdowns()
 
                             // Dismiss the modal
                             self.isModalPresented.toggle()
@@ -265,15 +391,13 @@ struct ModalView: View {
                 // Load reminder details if we are editing an existing reminder
                 if let reminder = reminderToEdit {
                     self.name = reminder.title
-                    self.startDate = reminder.startDate
                     self.startTime = reminder.startTime
-                    self.endDate = reminder.endDate ?? Date()
                     self.endTime = reminder.endTime ?? Date()
                     self.repeatEvery = reminder.repeatEvery
                     self.selectedRepeatOption = reminder.repeatUnit
                     self.selectedRepeatDays = reminder.repeatDays
                     self.isRepeatDaysOn = reminder.repeatDays.count > 0
-                    self.isEndDateAndTimeEnabled = reminder.endDate != nil ? true : false
+                    self.isEndTimeEnabled = reminder.endTime != nil ? true : false
                 }
             }
         }
